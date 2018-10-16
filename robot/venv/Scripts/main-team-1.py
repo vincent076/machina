da045aeedd136018afbf647229d3a5ff3f91cedf
Unknown <vincent@modify-miniatures.com>
2018-10-16 21:45:34 +0200

    seperate main.py for each team

---
+++ robot/venv/Scripts/main-team-1.py
@@ -0,0 +1,36 @@
+import sys
+from Objects.DriveMotor import DriveMotor
+from Objects.ArduinoBridge import ArduinoBridge
+from Objects.RobotData import RobotData
+from BehaviorManager import BehaviorManager
+import time
+
+# Create objects
+leftMotor = DriveMotor(35, 37)
+rightMotor = DriveMotor(36, 38)
+arduinoBridge = ArduinoBridge()
+
+
+#set team color to team 1
+arduinoBridge.setTeamColour(1)
+
+
+RobotData = RobotData(leftMotor, rightMotor, arduinoBridge)
+behaviorManager = BehaviorManager(RobotData)
+
+# start behavior
+behaviorManager.start()
+
+# wait on exit
+raw_input("Press Enter to exit")
+arduinoBridge.setTeamColour(RobotData.teamCol)
+#time.sleep(30)
+
+#stop all threads
+arduinoBridge.stop()
+behaviorManager.stop()
+sys.exit()
+
+
+
+
